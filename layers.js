075d0de034406f2bd03e59b732a43dbb10969c3c
nyameyei <nanayaanyameyei@gmail.com>
2024-10-30 02:24:14 +0000

    Update layers.js

--- layers.js
+++ layers.js
@@ -21,7 +21,7 @@ var lyr_GMRT_0 = new ol.layer.Tile({
         var lyr_OpenStreetMap_1 = new ol.layer.Tile({
             'title': 'OpenStreetMap',
             //'type': 'base',
-            'opacity': 1.000000,
+            'opacity': 0.500000,
             
             
             source: new ol.source.XYZ({
@@ -29,21 +29,8 @@ var lyr_GMRT_0 = new ol.layer.Tile({
                 url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
             })
         });
-var format_maskk_2 = new ol.format.GeoJSON();
-var features_maskk_2 = format_maskk_2.readFeatures(json_maskk_2, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_maskk_2 = new ol.source.Vector({
-    attributions: ' ',
-});
-jsonSource_maskk_2.addFeatures(features_maskk_2);
-var lyr_maskk_2 = new ol.layer.Vector({
-                declutter: false,
-                source:jsonSource_maskk_2, 
-                style: style_maskk_2,
-                popuplayertitle: "maskk",
-                interactive: false,
-                title: 'maskk'
-            });
+
+          
 var lyr_GPW_Population_Density_2020_3 = new ol.layer.Tile({
                             source: new ol.source.TileWMS(({
                               url: "https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi",
@@ -59,16 +46,7 @@ var lyr_GPW_Population_Density_2020_3 = new ol.layer.Tile({
                             
                           });
               wms_layers.push([lyr_GPW_Population_Density_2020_3, 1]);
-var group_maskgroup = new ol.layer.Group({
-                                layers: [lyr_OpenStreetMap_1,lyr_maskk_2,],
-                                fold: "open",
-                                title: "mask group"});
 
-lyr_GMRT_0.setVisible(true);lyr_OpenStreetMap_1.setVisible(true);lyr_maskk_2.setVisible(true);lyr_GPW_Population_Density_2020_3.setVisible(true);
-var layersList = [lyr_GMRT_0,group_maskgroup,lyr_GPW_Population_Density_2020_3];
-lyr_maskk_2.set('fieldAliases', {'fid': 'fid', });
-lyr_maskk_2.set('fieldImages', {'fid': '', });
-lyr_maskk_2.set('fieldLabels', {'fid': 'header label - always visible', });
-lyr_maskk_2.on('precompose', function(evt) {
-    evt.context.globalCompositeOperation = 'normal';
-});
+
+lyr_GMRT_0.setVisible(true);lyr_OpenStreetMap_1.setVisible(true);lyr_GPW_Population_Density_2020_3.setVisible(true);
+var layersList = [lyr_GMRT_0,lyr_GPW_Population_Density_2020_3];
